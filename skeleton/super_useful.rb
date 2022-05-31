9beda17796ffba1e83bc3460fa186664cd2d540a
mohammad_rizwan <rizwanm76@yahoo.com>
2022-05-31 17:08:15 -0400

    Commit after Bonus: Error Handling Funtime

--- skeleton/super_useful.rb
+++ skeleton/super_useful.rb
@@ -6,32 +6,55 @@ end
 # PHASE 3
 FRUITS = ["apple", "banana", "orange"]
 
+class CoffeeError < StandardError
+  def message
+    "No more caffeine!, please try again"
+  end
+end 
+
+class NotAFruitError < StandardError
+  def message
+    "That is not a fruit! I am leaving"
+  end
+end 
+
 def reaction(maybe_fruit)
   if FRUITS.include? maybe_fruit
     puts "OMG, thanks so much for the #{maybe_fruit}!"
-  else 
-    raise StandardError 
+  elsif maybe_fruit == "coffee" 
+    raise CoffeeError
+  else
+    raise NotAFruitError 
   end 
 end
 
 def feed_me_a_fruit
   puts "Hello, I am a friendly monster. :)"
-
+  begin 
   puts "Feed me a fruit! (Enter the name of a fruit:)"
   maybe_fruit = gets.chomp
-  reaction(maybe_fruit) 
+  reaction(maybe_fruit)
+  rescue CoffeeError => e
+    puts e.message
+    retry
+  rescue NotAFruitError => e
+    puts e.message 
+  end
 end  
 
 # PHASE 4
 class BestFriend
   def initialize(name, yrs_known, fav_pastime)
+    raise ArgumentError.new("years known must be greater than or equal to 5") if yrs_known.to_i < 5
+    raise ArgumentError.new("name cannot be blank, please enter a valid name") if name.empty?
+    raise ArgumentError.new("fav_pastime cannot be blank, please enter a valid fav_pastime") if fav_pastime.empty?
     @name = name
     @yrs_known = yrs_known
     @fav_pastime = fav_pastime
   end
 
   def talk_about_friendship
-    puts "Wowza, we've been friends for #{@yrs_known}. Let's be friends for another #{1000 * @yrs_known}."
+    puts "Wowza, we've been friends for #{@yrs_known} years. Let's be friends for another #{1000 * @yrs_known} years."
   end
 
   def do_friendstuff
